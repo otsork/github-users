681047637be29c2b0ca817e17ce81486172fe615
Otso Orkoneva <otso.orkoneva@gmail.com>
2020-10-20 21:36:46 +0300

    requestFn

---
+++ src/requests/requestFn.js
@@ -0,0 +1,66 @@
+import Axios from 'axios'
+
+const baseURL = 'https://api.github.com'
+
+class Request {
+  config
+  successCb
+  errorCb
+
+  constructor(args) {
+    const baseConfig = {
+      url: `${baseURL}${args.config.url}`,
+      params: args.config.params || {},
+      data: args.config.data || {}
+    }
+
+    this.config = {
+      ...baseConfig,
+      ...args.config
+    }
+
+    this.successCb = (response) => {
+      // console.log(response)
+      // const parsedResponse = this.config.parser && this.config.parser(response)
+      // console.log(parsedResponse)
+      if (args.successCb) args.successCb(response)
+      args.resolve(response)
+      return response
+    }
+
+    this.errorCb = (error) => {
+      if (args.errorCb) args.resolve(args.errorCb(error))
+      args.reject(error)
+      return error
+    }
+  }
+
+  async send() {
+    
+    console.log(this.config.url)
+    try {
+      const response = await Axios(this.config)
+      this.successCb(response)
+    } catch(error) {
+      this.errorCb(error)
+    }
+  }
+}
+
+export default async function requestFn(
+  config,
+  successCb,
+  errorCb,
+) {
+  return new Promise((resolve, reject) => {
+    const request = new Request({
+      config,
+      successCb,
+      errorCb,
+      resolve,
+      reject
+    })
+    console.log(request.config)
+    request.send()
+  })
+}
