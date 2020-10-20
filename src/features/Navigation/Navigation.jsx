831ce2c18c2c332ca6690eac7a865be4af24bc12
Otso Orkoneva <otso.orkoneva@gmail.com>
2020-10-20 22:11:58 +0300

    remove commented imports

--- src/features/Navigation/Navigation.jsx
+++ src/features/Navigation/Navigation.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 
 import { Button } from '@material-ui/core'
 
-// import actions from '../../actions'
 import { fetchTableData } from '../../requests/requests'
 
 
